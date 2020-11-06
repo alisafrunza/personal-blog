b9469caf07c374b53e1b083a4e2780bee24a9920
Alisa Frunza <a.frunza@youngcapital.nl>
2020-11-06 15:10:14 +0100

    Create tags pages

---
+++ src/pages/tags.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import PropTypes from "prop-types"
+import kebabCase from "lodash/kebabCase"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TagsPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => (
+  <Layout>
+    <SEO title="All Tags" />
+
+    <h1>All Tags</h1>
+
+    <ul>
+      {group.map(tag => (
+        <li key={tag.fieldValue}>
+          <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+            {tag.fieldValue} ({tag.totalCount})
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+TagsPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      group: PropTypes.arrayOf(
+        PropTypes.shape({
+          fieldValue: PropTypes.string.isRequired,
+          totalCount: PropTypes.number.isRequired,
+        }).isRequired
+      ),
+    }),
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }),
+    }),
+  }),
+}
+
+export default TagsPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
